0f741817907bb2e93c6b51a9c54658fd49e102f7
jswee2026 <joonseok.wee@gmail.com>
2026-02-27 18:03:03 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import os
+
+PARAMETERS = {
+   "lat": 41.8832,
+   "lon": -87.6324,
+   "appid": os.environ.get("OPEN_WEATHER_API_KEY"),
+   "cnt": 4
+}
+
+data = None
+need_umbrella = False
+
+try:
+   response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=PARAMETERS)
+   response.raise_for_status()
+   data = response.json()
+   print(data)
+except requests.exceptions.HTTPError as error:
+   print("Error occurred:", error)
+   data = {"error": str(error)}
+
+for list_item in data["list"]:
+   for weather_item in list_item["weather"]:
+       print("weather_item: ", weather_item)
+       if weather_item["id"] < 700:
+           need_umbrella = True
+
+print(need_umbrella)
+
+PARAMETERS_SMS = {
+   "phone": os.environ.get("NUMBER"),
+   "text": "Bring your umbrella.",
+   "apikey": os.environ.get("RON_API_KEY")
+}
+if need_umbrella:
+   response_sms = requests.get(url="https://api.callmebot.com/whatsapp.php", params=PARAMETERS_SMS)
